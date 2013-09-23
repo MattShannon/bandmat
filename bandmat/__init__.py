22f9fa98e248441644fb7dc2de228ab552a715ff
Matt Shannon <matt.shannon@cantab.net>
2013-09-23 17:52:23 +0100

    (add stuff from README to package-level docstring)
    Ignore-this: 9bb9c7eea59351749ada5967a19e3672
    
    darcs-hash:20130923165223-3a4db-0ba7241fd52e10653b2db074a5e21945aa458171

--- bandmat/__init__.py
+++ bandmat/__init__.py
@@ -1,4 +1,44 @@
-"""A banded matrix library for python."""
+"""A banded matrix library for python.
+
+This package provides a simple banded matrix library for python.
+It supports banded matrix-vector and matrix-matrix multiplication, converting
+between full and banded matrix representations, and certain linear algebra
+operations on banded matrices.
+
+Overview
+--------
+
+A banded matrix is a matrix where only the diagonal, a number of superdiagonals
+and a number of subdiagonals are non-zero.
+The well-known BLAS interface and LAPACK library for linear algebra define
+several banded matrix operations, and some of these, such as banded Cholesky
+decomposition, are wrapped in the excellent python package scipy, specifically
+in scipy.linalg.
+The bandmat package re-uses the banded matrix representation used by BLAS,
+LAPACK and scipy.linalg, wrapping it in a lightweight class for ease of use.
+See the docstring for the BandMat class for full details of the
+representation used.
+
+The bandmat package provides:
+
+- a lightweight class wrapping the LAPACK-style banded matrix representation.
+  This class keeps track of things like bandwidths to allow a more direct
+  coding style when working with banded matrices.
+- some basic banded matrix operations not present in scipy.
+  For example, banded matrix-vector multiplication is defined by BLAS but not
+  wrapped by scipy, and banded matrix-matrix multiplication is not defined in
+  BLAS or in scipy.
+  The bandmat package contains C implementations of these operations written in
+  cython.
+- helper functions for converting between full and banded matrix
+  representations.
+- certain linear algebra operations on banded matrices.
+- an implementation of a fancy indexed += function for numpy arrays.
+  This is included for (the author's) convenience and is not directly related
+  to banded matrix manipulation.
+
+Only square banded matrices are supported by this package.
+"""
 
 # Copyright 2013 Matt Shannon
 
