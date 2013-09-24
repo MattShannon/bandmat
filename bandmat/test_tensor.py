b5bae0e699d63bf2ae43db961f400147d993d5c4
Matt Shannon <matt.shannon@cantab.net>
2013-09-24 12:29:37 +0100

    (tensor - update test code to the style I now slightly prefer)
    Ignore-this: a5e255fea9b855f224537d82dd0056e0
    
    darcs-hash:20130924112937-3a4db-5198083f4d65df712afca6e36ffd3e3cc31e0fc3

--- bandmat/test_tensor.py
+++ bandmat/test_tensor.py
@@ -25,13 +25,12 @@ class TestTensor(unittest.TestCase):
             size = random.choice([0, 1, randint(0, 10), randint(0, 100)])
             a_bm = gen_BandMat(size)
             b = randn(size)
-            c_orig = randn(size)
+            c = randn(size)
+            a_full = a_bm.full()
+            c_good = c.copy()
 
-            c = c_orig.copy()
             bm.dot_mv_plus_equals(a_bm, b, c)
-
-            c_good = c_orig.copy()
-            c_good += np.dot(a_bm.full(), b)
+            c_good += np.dot(a_full, b)
             assert_allclose(c, c_good)
 
     def test_dot_mv(self, its = 100):
@@ -39,10 +38,10 @@ class TestTensor(unittest.TestCase):
             size = random.choice([0, 1, randint(0, 10), randint(0, 100)])
             a_bm = gen_BandMat(size)
             b = randn(size)
+            a_full = a_bm.full()
 
             c = bm.dot_mv(a_bm, b)
-
-            c_good = np.dot(a_bm.full(), b)
+            c_good = np.dot(a_full, b)
             assert_allclose(c, c_good)
 
     def test_dot_mm_plus_equals(self, its = 50):
@@ -50,24 +49,21 @@ class TestTensor(unittest.TestCase):
             size = random.choice([0, 1, randint(0, 10), randint(0, 100)])
             a_bm = gen_BandMat(size)
             b_bm = gen_BandMat(size)
-            c_bm_orig = gen_BandMat(size)
+            c_bm = gen_BandMat(size)
             diag = None if randBool() else randn(size)
+            diag_value = np.ones((size,)) if diag is None else diag
+            a_full = a_bm.full()
+            b_full = b_bm.full()
+            c_full = c_bm.full()
+            l = c_bm.l
+            u = c_bm.u
 
-            c_bm = c_bm_orig.copy_exact()
             bm.dot_mm_plus_equals(a_bm, b_bm, c_bm, diag = diag)
-
-            c_full_good = c_bm_orig.full()
-            c_full_good += fl.band_ec(
-                c_bm.l, c_bm.u,
-                np.dot(
-                    np.dot(
-                        a_bm.full(),
-                        np.diag(np.ones((size,)) if diag is None else diag)
-                    ),
-                    b_bm.full()
-                )
+            c_full += fl.band_ec(
+                l, u,
+                np.dot(np.dot(a_full, np.diag(diag_value)), b_full)
             )
-            assert_allclose(c_bm.full(), c_full_good)
+            assert_allclose(c_bm.full(), c_full)
 
     def test_dot_mm(self, its = 50):
         for it in range(its):
@@ -75,20 +71,16 @@ class TestTensor(unittest.TestCase):
             a_bm = gen_BandMat(size)
             b_bm = gen_BandMat(size)
             diag = None if randBool() else randn(size)
+            diag_value = np.ones((size,)) if diag is None else diag
+            a_full = a_bm.full()
+            b_full = b_bm.full()
 
             c_bm = bm.dot_mm(a_bm, b_bm, diag = diag)
+            c_full = np.dot(np.dot(a_full, np.diag(diag_value)), b_full)
             assert c_bm.l == a_bm.l + b_bm.l
             assert c_bm.u == a_bm.u + b_bm.u
             assert c_bm.size == size
-
-            c_full_good = np.dot(
-                np.dot(
-                    a_bm.full(),
-                    np.diag(np.ones((size,)) if diag is None else diag)
-                ),
-                b_bm.full()
-            )
-            assert_allclose(c_bm.full(), c_full_good)
+            assert_allclose(c_bm.full(), c_full)
 
     def test_dot_mm_partial(self, its = 50):
         for it in range(its):
@@ -98,23 +90,19 @@ class TestTensor(unittest.TestCase):
             l = random.choice([0, 1, randint(0, 10)])
             u = random.choice([0, 1, randint(0, 10)])
             diag = None if randBool() else randn(size)
+            diag_value = np.ones((size,)) if diag is None else diag
+            a_full = a_bm.full()
+            b_full = b_bm.full()
 
             c_bm = bm.dot_mm_partial(l, u, a_bm, b_bm, diag = diag)
+            c_full = fl.band_ec(
+                l, u,
+                np.dot(np.dot(a_full, np.diag(diag_value)), b_full)
+            )
             assert c_bm.l == l
             assert c_bm.u == u
             assert c_bm.size == size
-
-            c_full_good = fl.band_ec(
-                l, u,
-                np.dot(
-                    np.dot(
-                        a_bm.full(),
-                        np.diag(np.ones((size,)) if diag is None else diag)
-                    ),
-                    b_bm.full()
-                )
-            )
-            assert_allclose(c_bm.full(), c_full_good)
+            assert_allclose(c_bm.full(), c_full)
 
     def test_dot_mmm_partial(self, its = 50):
         for it in range(its):
@@ -124,38 +112,31 @@ class TestTensor(unittest.TestCase):
             c_bm = gen_BandMat(size)
             l = random.choice([0, 1, randint(0, 10)])
             u = random.choice([0, 1, randint(0, 10)])
+            a_full = a_bm.full()
+            b_full = b_bm.full()
+            c_full = c_bm.full()
 
             d_bm = bm.dot_mmm_partial(l, u, a_bm, b_bm, c_bm)
+            d_full = fl.band_ec(l, u, np.dot(a_full, np.dot(b_full, c_full)))
             assert d_bm.l == l
             assert d_bm.u == u
             assert d_bm.size == size
-
-            d_full_good = fl.band_ec(
-                l, u,
-                np.dot(
-                    a_bm.full(),
-                    np.dot(b_bm.full(), c_bm.full())
-                )
-            )
-            assert_allclose(d_bm.full(), d_full_good)
+            assert_allclose(d_bm.full(), d_full)
 
     def test_band_of_outer_plus_equals(self, its = 50):
         for it in range(its):
             size = random.choice([0, 1, randint(0, 10), randint(0, 100)])
-            mat_bm_orig = gen_BandMat(size)
             a_vec = randn(size)
             b_vec = randn(size)
             mult = randn()
+            mat_bm = gen_BandMat(size)
+            mat_full = mat_bm.full()
+            l = mat_bm.l
+            u = mat_bm.u
 
-            mat_bm = mat_bm_orig.copy_exact()
             bm.band_of_outer_plus_equals(a_vec, b_vec, mat_bm, mult = mult)
-
-            mat_full_good = mat_bm_orig.full()
-            mat_full_good += fl.band_ec(
-                mat_bm.l, mat_bm.u,
-                np.outer(a_vec, b_vec) * mult
-            )
-            assert_allclose(mat_bm.full(), mat_full_good)
+            mat_full += fl.band_ec(l, u, np.outer(a_vec, b_vec) * mult)
+            assert_allclose(mat_bm.full(), mat_full)
 
 if __name__ == '__main__':
     unittest.main()
