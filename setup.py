abffa361799754d43ab9117d074c363f5ad999b2
Matt Shannon <matt.shannon@cantab.net>
2013-10-17 02:56:26 +0100

    prepare to release 0.3
    Ignore-this: 6b847455a0aa2fb7fef6833480006155
    
    darcs-hash:20131017015626-3a4db-3e6f6de680de03c86538aec45fb1a7c2f45d8ff9

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ else:
 
 setup(
     name = 'bandmat',
-    version = '0.3.dev1',
+    version = '0.3',
     description = 'A banded matrix library for python.',
     url = 'http://github.com/MattShannon/bandmat',
     author = 'Matt Shannon',
