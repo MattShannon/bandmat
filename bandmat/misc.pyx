a843af24dadb813a7186a69fcf8910dcaec0b24b
Matt Shannon <matt.shannon@cantab.net>
2014-09-18 15:48:07 +0100

    misc - add fancy_plus_equals_3d
    Ignore-this: 332272c70e2e774e57b9f66bcbc37a2b
    
    darcs-hash:20140918144807-3a4db-ca1e6cf77eb6d1691bc088405fd9346a77bbdac4

--- bandmat/misc.pyx
+++ bandmat/misc.pyx
@@ -61,3 +61,38 @@ def fancy_plus_equals_2d(cnp.ndarray[cnp.int_t, ndim=1] target_index_seq,
             target[target_index, index1] += source[source_index, index1]
 
     return
+
+def fancy_plus_equals_3d(cnp.ndarray[cnp.int_t, ndim=1] target_index_seq,
+                         cnp.ndarray[cnp.float64_t, ndim=3] source,
+                         cnp.ndarray[cnp.float64_t, ndim=3] target):
+    """Implements a += method with fancy indexing.
+
+    Does what you might expect
+        target[target_index_seq] += source
+    to do.
+    """
+    cdef unsigned long source_size
+    cdef unsigned long size1
+    cdef unsigned long size2
+
+    source_size = source.shape[0]
+    assert target_index_seq.shape[0] == source_size
+    size1 = source.shape[1]
+    assert target.shape[1] == size1
+    size2 = source.shape[2]
+    assert target.shape[2] == size2
+
+    cdef unsigned long source_index
+    cdef long target_index
+    cdef unsigned long index1
+    cdef unsigned long index2
+
+    for source_index in range(source_size):
+        target_index = target_index_seq[source_index]
+        for index1 in range(size1):
+            for index2 in range(size2):
+                target[target_index, index1, index2] += (
+                    source[source_index, index1, index2]
+                )
+
+    return
--- bandmat/test_misc.py
+++ bandmat/test_misc.py
@@ -8,6 +8,7 @@
 from bandmat.testhelp import assert_allclose, get_array_mem
 
 from bandmat.misc import fancy_plus_equals, fancy_plus_equals_2d
+from bandmat.misc import fancy_plus_equals_3d
 
 import unittest
 import random
@@ -83,6 +84,43 @@ class TestMisc(unittest.TestCase):
             )
             self.assertRaises(IndexError,
                               fancy_plus_equals_2d,
+                              target_index_seq, source, target)
+
+    def test_fancy_plus_equals_3d(self, its=100):
+        for it in range(its):
+            source_size = random.choice([0, 1, randint(10), randint(100)])
+            target_size = random.choice([1, randint(1, 10), randint(1, 100)])
+            size1 = random.choice([0, 1, randint(10)])
+            size2 = random.choice([0, 1, randint(10)])
+            source = randn(source_size, size1, size2)
+            target = randn(target_size, size1, size2)
+            target_index_seq = randint(target_size, size=source_size)
+            array_mem = get_array_mem(target_index_seq, source, target)
+
+            target_good = target.copy()
+            for source_index, target_index in enumerate(target_index_seq):
+                target_good[target_index] += source[source_index]
+
+            fancy_plus_equals_3d(target_index_seq, source, target)
+
+            assert_allclose(target, target_good)
+            assert get_array_mem(target_index_seq, source, target) == array_mem
+
+    def test_fancy_plus_equals_3d_fails(self, its=100):
+        for it in range(its):
+            source_size = random.choice([1, randint(1, 10), randint(1, 100)])
+            target_size = random.choice([1, randint(1, 10), randint(1, 100)])
+            size1 = random.choice([1, randint(1, 10)])
+            size2 = random.choice([1, randint(1, 10)])
+            source = randn(source_size, size1, size2)
+            target = randn(target_size, size1, size2)
+            target_index_seq = randint(target_size, size=source_size)
+            target_index_seq[randint(source_size)] = (
+                (-target_size - 1 - randint(10)) if rand_bool()
+                else target_size + randint(10)
+            )
+            self.assertRaises(IndexError,
+                              fancy_plus_equals_3d,
                               target_index_seq, source, target)
 
 if __name__ == '__main__':
