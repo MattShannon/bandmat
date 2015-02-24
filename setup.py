574faad4c66e0a039ea65ad68cf9e4dd4f3e2e88
Matt Shannon <matt.shannon@cantab.net>
2015-02-24 18:42:24 +0000

    (add compiler flag to prevent log noise during compilation)
    Ignore-this: ef9a8784a7b89552d91c5d63a53d7ba1
    
    darcs-hash:20150224184224-3a4db-737cb20da434ca471cf351c00b7599dfc3e5bec1

--- setup.py
+++ setup.py
@@ -41,12 +41,18 @@ if dev_mode:
             _sdist.run(self)
 
     cmdclass = {'build_ext': build_ext, 'sdist': sdist}
-    ext_modules = [ Extension('.'.join(loc), [os.path.join(*loc)+'.pyx'])
-                    for loc in cython_locs ]
+    ext_modules = [
+        Extension('.'.join(loc), [os.path.join(*loc)+'.pyx'],
+                  extra_compile_args=['-Wno-unused-but-set-variable'])
+        for loc in cython_locs
+    ]
 else:
     cmdclass = {}
-    ext_modules = [ Extension('.'.join(loc), [os.path.join(*loc)+'.c'])
-                    for loc in cython_locs ]
+    ext_modules = [
+        Extension('.'.join(loc), [os.path.join(*loc)+'.c'],
+                  extra_compile_args=['-Wno-unused-but-set-variable'])
+        for loc in cython_locs
+    ]
 
 setup(
     name='bandmat',
