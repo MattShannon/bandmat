339abc3ecdaa7b3661907c57beddda9d9a78e074
Lucian Zala <zala.lucian@gmail.com>
2018-04-18 01:32:33 +0300

    Improved setup file for installing numpy and cython
    - This change helps when installing the module as
    part of the requirements of another module and
    numpy, cython are missing.
    
    Signed-off-by: Lucian Zala <zala.lucian@gmail.com>

--- setup.py
+++ setup.py
@@ -7,10 +7,10 @@
 # See `License` for details of license and warranty.
 
 import os
-import numpy as np
 from setuptools import setup
 from setuptools.extension import Extension
 from setuptools.command.sdist import sdist as _sdist
+from setuptools.command.build_ext import build_ext as _build_ext
 
 cython_locs = [
     ('bandmat', 'full'),
@@ -24,40 +24,52 @@ cython_locs = [
 with open('README.rst') as readme_file:
     long_description = readme_file.read()
 
-requires = [ line.rstrip('\n') for line in open('requirements.txt') ]
+requires = [line.rstrip('\n') for line in open('requirements.txt')]
+
+src_ext = '.c'
+cmdclass = {}
 
 # see "A note on setup.py" in README.rst for an explanation of the dev file
 dev_mode = os.path.exists('dev')
 
 if dev_mode:
-    from Cython.Distutils import build_ext
-    from Cython.Build import cythonize
+    src_ext = '.pyx'
 
-    class sdist(_sdist):
+
+    class Sdist(_sdist):
+        def __init__(self, *args, **kwargs):
+            from Cython.Build import cythonize
+            _sdist.__init__(self, *args, **kwargs)
+
         """A cythonizing sdist command.
 
         This class is a custom sdist command which ensures all cython-generated
         C files are up-to-date before running the conventional sdist command.
         """
         def run(self):
-            cythonize([ os.path.join(*loc)+'.pyx' for loc in cython_locs ])
+            cythonize([os.path.join(*loc) + src_ext for loc in cython_locs])
             _sdist.run(self)
 
-    cmdclass = {'build_ext': build_ext, 'sdist': sdist}
-    ext_modules = [
-        Extension('.'.join(loc), [os.path.join(*loc)+'.pyx'],
-                  extra_compile_args=['-Wno-unused-but-set-variable', '-O3'],
-                  include_dirs=[np.get_include()])
-        for loc in cython_locs
-    ]
-else:
-    cmdclass = {}
-    ext_modules = [
-        Extension('.'.join(loc), [os.path.join(*loc)+'.c'],
-                  extra_compile_args=['-Wno-unused-but-set-variable', '-O3'],
-                  include_dirs=[np.get_include()])
-        for loc in cython_locs
-    ]
+    cmdclass['sdist'] = Sdist
+
+
+class BuildExt(_build_ext):
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        # Prevent numpy from thinking it is still in its setup process
+        __builtins__.__NUMPY_SETUP__ = False
+
+        import numpy as np
+        self.include_dirs.append(np.get_include())
+
+
+cmdclass['build_ext'] = BuildExt
+
+ext_modules = [
+    Extension('.'.join(loc), [os.path.join(*loc) + src_ext],
+              extra_compile_args=['-Wno-unused-but-set-variable', '-O3'])
+    for loc in cython_locs
+]
 
 setup(
     name='bandmat',
@@ -68,6 +80,7 @@ setup(
     author_email='matt.shannon@cantab.net',
     license='3-clause BSD (see License file)',
     packages=['bandmat'],
+    setup_requires=[requires[0:2]],
     install_requires=requires,
     long_description=long_description,
     cmdclass=cmdclass,
